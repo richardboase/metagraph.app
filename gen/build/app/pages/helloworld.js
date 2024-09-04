df2983035e10e3b20bbd5e818c8577c37f5ba91f
richardboase <richardwboase@gmail.com>
2024-09-04 20:54:45 +0100

    hellow2

---
+++ gen/build/app/pages/helloworld.js
@@ -0,0 +1,12 @@
+// helloworld.js
+import React from 'react';
+
+const HelloWorld2 = () => {
+    return (
+        <div>
+            <h1>Hello, World 2!</h1>
+        </div>
+    );
+};
+
+export default HelloWorld2;
